4537bc32bdd5f2b04b3007f7e6b4511b3f449290
Zack Tan <Zack.Tan@man.com>
2022-10-17 11:29:12 +0100

    ExtensionMethod

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator;
         
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices
+                .GetService<IMediator>();
     }
 }
